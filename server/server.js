c3cd0b595d13974917454e54f1e5ec5730e40fd4
Nat <n3rodriguez2001@gmail.com>
2024-01-14 01:54:09 -0500

    attempting to run on port 3000 because of issues

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { expressMiddleware } = require('@apollo/server/express4');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const server = new ApolloServer({
   typeDefs,
   resolvers
